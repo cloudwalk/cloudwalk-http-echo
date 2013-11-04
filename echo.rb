9f7020f8932e1d5a616cd84256c2e9ef2aee12ac
Bruno Freitas <bmsatierf@gmail.com>
2013-11-04 15:37:47 -0200

    Adds bind 0.0.0.0

--- echo.rb
+++ echo.rb
@@ -1,6 +1,11 @@
 require "rubygems"
 require "sinatra"
 
+#Bound to this address so that external hosts can access it, VERY IMPORTANT!
+set :bind, '0.0.0.0'
+
+set :logging, true
+
 get '/' do
   redirect "http://docs.cloudwalk.io"
 end
